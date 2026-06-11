2f59c978758df7b8724b0c6a58436f4ef34413f5
fran-dev <whitehat94@hotmail.com>
2026-06-11 08:42:43 -0600

    favicon

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Protected routes
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|icon.png|icon.svg|api|trpc).*)',
   ],
-}
+};
